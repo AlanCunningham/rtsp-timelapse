b44795829fe6eca6bd07c41fd66583823086eceb
Alan Cunningham <ajc.alan@gmail.com>
2022-11-09 22:37:17 +0000

    Remove while True loop when taking a photo from the stream

--- main.py
+++ main.py
@@ -10,18 +10,16 @@ def main():
     rtsp_ip_address = config.rtsp_ip_address
     rtsp_path = f"rtsp://{rtsp_username}:{rtsp_password}@{rtsp_ip_address}/stream1"
 
-    while True:
-        subprocess.run(
-            [
-                "ffmpeg",
-                "-i",
-                rtsp_path,  # Input path
-                "-vframes",
-                "1",  # Only grab one frame
-                f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.png",  # Output file
-            ]
-        )
-        time.sleep(5)
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            rtsp_path,  # Input path
+            "-vframes",
+            "1",  # Only grab one frame
+            f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.png",  # Output file
+        ]
+    )
 
 
 if __name__ == "__main__":
