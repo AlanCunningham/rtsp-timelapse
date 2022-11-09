1ac1bd37ea0a409c6936e006372462e3e4395afa
Alan Cunningham <ajc.alan@gmail.com>
2022-11-09 22:41:20 +0000

    Add comments for ffmpeg when we get the frame

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ def main():
     rtsp_ip_address = config.rtsp_ip_address
     rtsp_path = f"rtsp://{rtsp_username}:{rtsp_password}@{rtsp_ip_address}/stream1"
 
+    # Use ffmpeg to connect to the rtsp stream and save 1 frame
+    # ffmpeg -i <stream> -vframes 1 <output>
     subprocess.run(
         [
             "ffmpeg",
             "-i",
             rtsp_path,
             "-vframes",
-            "1",  # Only grab one frame
-            f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.png",  # Output file
+            "1",
+            f"{datetime.now().strftime('%Y%m%d-%H%M%S')}.png",
         ]
     )
 
